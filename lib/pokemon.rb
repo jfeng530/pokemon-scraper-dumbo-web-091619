1b920573fbed58ba63234ef97cd2a741f0f9115e
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-26 21:09:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,6 +3,11 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id, :db
   
-  def initialize(id= nil)
+  def initialize(id= nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end 
   
 end
